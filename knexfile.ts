4a8f8dfafd570ddae821c98dfd81a34998e0fed1
Marcos Botene <marcosjbotene@gmail.com>
2023-12-02 14:32:51 -0300

    chore: created knexfile.ts

---
+++ knexfile.ts
@@ -0,0 +1,41 @@
+import type { Knex } from 'knex';
+
+import { config as developmentConfig } from './src/database';
+
+const config: { [key: string]: Knex.Config } = {
+  development: developmentConfig,
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
+
+module.exports = config;
