c11fbdbc7450e7237f4da645fd94662faed17fd9
Marcos Botene <marcosjbotene@gmail.com>
2023-12-02 14:32:09 -0300

    refactor: created config object

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,14 @@
-import { knex as setupKnex } from 'knex';
+import { Knex, knex as setupKnex } from 'knex';
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'sqlite3',
-  connection: { filename: ':memory:' },
+  connection: { filename: './db/app.db' },
   useNullAsDefault: true,
-});
+  migrations: {
+    directory: './db/migrations',
+    database: 'sqlite3',
+    extension: 'ts',
+  },
+};
+
+export const knex = setupKnex(config);
