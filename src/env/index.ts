832675ab9495cbcb8708ec9703bbe7621b86d10e
Marcos Botene <marcosjbotene@gmail.com>
2023-12-02 22:39:32 -0300

    fix: env validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,10 @@ import { z } from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z
+    .string()
+    .default('3333')
+    .transform(port => Number(port)),
 });
 
 export const env = envSchema.parse(process.env);
